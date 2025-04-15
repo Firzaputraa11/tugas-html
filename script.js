9b1d32258e0d233e19dd80520624dfe15d9510d3
Munequita04 <Firzaekaputra.11@gmail.com>
2025-04-15 14:31:54 +0800

    Mengupdate semua file untuk tugas mid-project

--- script.js
+++ script.js
@@ -44,4 +44,10 @@ const imageSets = [
       });
     });
   });
+  function updateClock() {
+    const now = new Date();
+    document.getElementById("clock").textContent = now.toLocaleTimeString();
+  }
+  setInterval(updateClock, 1000);
+  updateClock();
   
